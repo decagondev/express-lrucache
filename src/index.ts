aa22c379a7063de49a2a5a9ccc51e295d424f729
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-05 02:38:01 +0000

    added sqlite table and create table

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import express, { Express, Request, Response } from 'express';
+import sqlite3 from 'sqlite3';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 5555;
+app.use(express.json());
+
+
+const db = new sqlite3.Database('sounds.db');
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS sounds (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    idKey TEXT NOT NULL,
+    audio TEXT NOT NULL
+  )
+`);
 
 
 
